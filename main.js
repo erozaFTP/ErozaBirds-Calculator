7f1df581c68f736705a6ce3a8cc0c89335e4f146
Jim milton <adampigeons@gmail.com>
2019-11-01 10:09:55 -0700

    Added js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('testing test tesst will it push??');
+/*----------------*/ 
+console.log('test');
+/*----------------*/
+/*=-===-=*/ 
+
+const numberButtons = document.querySelectorAll('[data-number]');/*this gets all our custom attributes with buttons given "data-number" name from html */ 
+ 
+
+const operationButtons = document.querySelectorAll('[data-operation]');
+/*this one gets attributes with "data-operation"name*/ 
+
+const equalsButton =document.querySelector('[data-equals]');
+
+const deleteButton 
+
+
+
+
