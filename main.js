4bbf5a75c8c5476198a47d68076098ed35100c78
Jim milton <adampigeons@gmail.com>
2019-11-01 10:25:47 -0700

    js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const deleteButton =document.querySelector('[data-delete');
 
 const allClearButton = document.querySelector('[data-all-clear]');
 
-const previousOperandTextElement = document.querySelector(['data-previous-operand']);
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 
 const currentOperandTextElement =document.querySelector('[data-current-operand]');
 
