f76cac486871cf411003603216d70f6c8ab0d626
Jim milton <adampigeons@gmail.com>
2019-11-01 10:19:58 -0700

    more js

--- main.js
+++ main.js
@@ -11,7 +11,13 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 
 const equalsButton =document.querySelector('[data-equals]');
 
-const deleteButton 
+const deleteButton =document.querySelector('[data-delete');
+
+const allClearButton = document.querySelector('[data-all-clear]');
+
+const previousOperandTextElement = document.querySelector(['data-previous-operand']);
+
+const currentOperandTextElement =document.querySelector('[data-current-operand]');
 
 
 
