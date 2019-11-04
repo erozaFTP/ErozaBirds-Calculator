16bc5482c6b695e22fe92bd2c570ec3ba91ce97b
Jim milton <adampigeons@gmail.com>
2019-11-04 05:15:15 -0800

    js with notes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ console.log('test');
 
 
 
-/* 1. FIRST THiNG THAT WAS DONE WAS TO GET ALL THE BUTTONS.
+/* STEP 1. FIRST THiNG THAT WAS DONE WAS TO GET ALL THE BUTTONS. 
 
 .SIDENOTE::i've asked 
 "I know how to read code but don't know how to put 
@@ -29,5 +29,30 @@ const currentOperandTextElement =document.querySelector('[data-current-operand]'
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 /*--------------------------END OF THE 1ST STEP--------------------------------------------*/ 
+/*=-=-==-=--=-==-=--=-=-=-=-=-=-=-=-=--=-==--=-==-=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=*/
+
+/*STEP 2 MAKING A CLASS-THE MOST CRUCIAL PART FOR THE FUNCTIONALITY OF THE CALCULATOR
+NOW WHAT IS A CLASS? you might ask>? which is a totally reasonable question especially if javascript
+is your first language to learn   (most programming languages have classes) (in es5'ecmascript 5' 
+it is made to emulate class like functionality);
+Classes are blueprints or templates that be used to create new objects from. They hold 
+information, details, about an object.
+more in depth guide can be found here https://www.youtube.com/watch?v=8yjkWGRlUmY  - CS Dojo
+with 1.13m subs a
+*/ 
+
+
+class Calculator{
+
+constructor(prev){
+  /*constructors sort of like a function */ 
+  
+
+
+}
+
+}
+
+
 
 
